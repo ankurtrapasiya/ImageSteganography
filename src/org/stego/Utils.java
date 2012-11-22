9c27c2c13606dd3e9fd3739e9251153803377d67
Updateankur.trapasiya <ankur.trapasiya@gmail.com>
2012-11-22 17:41:13 +0530

    Image Stegano reveal code added.

--- src/org/stego/Utils.java
+++ src/org/stego/Utils.java
@@ -8,8 +8,10 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,113 +22,113 @@ import javax.imageio.ImageIO;
  * @author ankur
  */
 public class Utils {
-    
+
     private static final int DATA_SIZE = 8;
     private static final int MAX_INT_LEN = 4;
-    
+
     public static byte[] buildStego(String inputText) {
         byte[] stego = null;
-        
-        
+
+
         byte[] msgBytes = inputText.getBytes();
         byte[] dataLength = intToBytes(msgBytes.length);
-        
+
         int totalLen = dataLength.length + msgBytes.length;
-        
+
         stego = new byte[totalLen];
-        
+
         System.arraycopy(dataLength, 0, stego, 0, dataLength.length);
-        
+
         System.arraycopy(msgBytes, 0, stego, dataLength.length, msgBytes.length);
-        
+
         return stego;
     }
-    
+
     private static byte[] intToBytes(int i) {
         byte[] byteArr = new byte[MAX_INT_LEN];
-        
+
         byteArr[0] = (byte) ((i >>> 24) & 0xFF);
         byteArr[1] = (byte) ((i >>> 16) & 0xFF);
         byteArr[2] = (byte) ((i >>> 8) & 0xFF);
         byteArr[3] = (byte) (i & 0xFF);
-        
+
         return byteArr;
     }
-    
+
     private static byte[] accessBytes(BufferedImage image) {
         WritableRaster raster = image.getRaster();
         DataBufferByte buffer = (DataBufferByte) raster.getDataBuffer();
-        
+
         return buffer.getData();
     }
-    
+
     private static boolean singleHide(byte[] imBytes, byte[] stego) {
         int imageLength = imBytes.length;
-        
+
         int totalLength = stego.length;
-        
+
         if ((totalLength * DATA_SIZE) > imageLength) {
             System.out.println("too big message for image...oops");
             return false;
         }
-        
+
         hideStego(imBytes, stego, 0);
         return true;
     }
-    
+
     private static void hideStego(byte[] imBytes, byte[] stego, int offset) {
-        
+
         for (int i = 0; i < stego.length; i++) {
-            
+
             int byteVal = stego[i];
-            
+
             for (int j = 7; j >= 0; j--) {
                 int bitVal = (byteVal >>> j) & 1;
-                
+
                 imBytes[offset] = (byte) ((imBytes[offset] & 0xFE) | bitVal);
-                
+
                 offset++;
-                
+
             }
         }
     }
-    
+
     public static boolean hide(String fileName, String imageFileName) {
-        
+
         String inputText = readTextFile(fileName);
-        
+
         if (inputText.length() == 0) {
             return false;
         }
-        
+
         byte[] stego = buildStego(inputText);
         try {
-            
+
             BufferedImage image = ImageIO.read(new File(imageFileName));
-            
+
             if (image == null) {
                 return false;
             }
-            
+
             byte[] imageBytes = accessBytes(image);
-            
-            
+
+
             if (!singleHide(imageBytes, stego)) {
                 return false;
             }
-            
+
             String outputFileName = "abc.png";
-            
+
             return writeImageToFile(outputFileName, image);
         } catch (IOException ex) {
             Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return true;
     }
-    
+
     private static boolean writeImageToFile(String fileName, BufferedImage image) {
-        
+
         File file = new File(fileName);
         try {
             ImageIO.write(image, "png", file);
@@ -136,25 +138,133 @@ public class Utils {
             return false;
         }
     }
-    
+
     private static String readTextFile(String fileName) {
-        
+
         StringBuilder text = new StringBuilder();
-        
+
         try {
-            
+
             BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
             String str = null;
             while ((str = br.readLine()) != null) {
-                
+
                 text.append(str);
-                
+
             }
-            
+
         } catch (IOException ex) {
             Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return text.toString();
+    }
+
+    private static boolean writeStringToFile(String fileName, String message) {
+        File file = new File(fileName);
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.write(message);
+            return true;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean reveal(String fileName) {
+        try {
+
+            BufferedImage bi = ImageIO.read(new File(fileName));
+
+            if (bi == null) {
+                return false;
+            }
+
+            byte[] imageBytes = accessBytes(bi);
+
+            int msgLength = getMsgLength(imageBytes, 0);
+
+            if (msgLength == -1) {
+                return false;
+            }
+
+            String msg = getMessage(imageBytes, msgLength, MAX_INT_LEN * DATA_SIZE);
+
+            if (msg != null) {
+
+                writeStringToFile("msg.txt", msg);
+
+            } else {
+                System.out.println("No message found");
+                return false;
+            }
+
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+        }
+        return true;
+
+    }
+
+    private static int getMsgLength(byte[] imageBytes, int offset) {
+
+        byte[] lenBytes = extractHiddenBytes(imageBytes, MAX_INT_LEN, offset);
+
+        if (lenBytes == null) {
+            return -1;
+        }
+
+        int msgLength = ((lenBytes[0] & 0xff) << 24)
+                | ((lenBytes[0] & 0xff) << 16)
+                | ((lenBytes[0] & 0xff) << 8)
+                | (lenBytes[0] & 0xff);
+
+        if (msgLength <= 0 || msgLength > imageBytes.length) {
+            System.out.println("incorrect message length");
+            return -1;
+        }
+        return msgLength;
+    }
+
+    private static byte[] extractHiddenBytes(byte[] imageBytes, int size, int offset) {
+
+        int finalPosition = offset + (size * DATA_SIZE);
+
+        if (finalPosition > imageBytes.length) {
+            System.out.println("image end reached");
+            return null;
+        }
+
+        byte[] hiddenBytes = new byte[size];
+
+
+        for (int j = 0; j < size; j++) {
+            for (int i = 0; i < DATA_SIZE; i++) {
+                hiddenBytes[j] = (byte) ((hiddenBytes[j] << 1) | (imageBytes[offset] & 1));
+
+                offset++;
+            }
+        }
+
+        return hiddenBytes;
+    }
+
+    private static String getMessage(byte[] imageBytes, int msgLength, int offset) {
+        byte[] msgBytes = extractHiddenBytes(imageBytes, msgLength, offset);
+
+        if (msgBytes == null) {
+            return null;
+        }
+
+        String msg = new String(msgBytes);
+
+//        if(isPrintable(msg))
+//            return msg;
+//        else return null;
+
+        return msg;
     }
 }
---
+++ test/org/stego/UtilsTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.stego;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ankur
+ */
+public class UtilsTest {
+
+    public UtilsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of buildStego method, of class Utils.
+     */
+    @Test
+    public void testBuildStego() {
+        System.out.println("buildStego");
+        String inputText = "";
+        byte[] expResult = null;
+        byte[] result = Utils.buildStego(inputText);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of hide method, of class Utils.
+     */
+    @Test
+    public void testHide() {
+        System.out.println("hide");
+        String fileName = "/home/ankur/NetBeansProjects/ImageSteganography/image/t1.txt";
+        String imageFileName = "/home/ankur/NetBeansProjects/ImageSteganography/image/14.png";
+        boolean expResult = true;
+        boolean result = Utils.hide(fileName, imageFileName);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+}
